bd36d98df3a1621d352eac0b3fe911ddff9306ea
WesleyEdwards <westheedwards@gmail.com>
2023-10-07 14:49:37 -0600

    new build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const removeModuleCrossorigin = () => {
+  return {
+    name: "no-attribute",
+    transformIndexHtml(html: string) {
+      return html.replace(`type="module" crossorigin `, "");
+    },
+  };
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // plugins: [react(), removeModuleCrossorigin()],
+  base: "./",
+});
